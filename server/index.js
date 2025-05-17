445ddecdecede75de09ee68bf5a65a97d08bdb01
Mohit Agrawal <mohitagrawal@Mohits-MacBook-Air.local>
2025-05-17 14:30:05 +0530

    Deployment changes

--- server/index.js
+++ server/index.js
@@ -219,14 +219,8 @@ app.get('/courses/enrollments', async (req, res) => {
   }
 });
 
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-
-// The "catchall" handler: for any request that doesn't
-// match your API routes, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
 });
